696131b091572b32dd3b077b324de645f08a58f6
masa950724mi <s100.iizuka.masami@gmail.com>
2023-07-19 18:49:46 +0900

    Initial Commit

---
+++ QiitaAPIProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QiitaAPIProject
+//
+//  Created by 飯塚政美 on 2023/07/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
